df18280d3c9f8a5373aa22ba37165a082c14512b
JagdeepZenith <98079382+JagdeepZenith@users.noreply.github.com>
2025-11-06 12:30:04 +0530

    a

--- index.js
+++ index.js
@@ -5,68 +5,81 @@ const axios = require('axios');
 
 app.use(express.json());
 
-app.get('/liverates/:key', async(req,res)=>{
-    if(req.params.key==='f2PqY7RjVh6mKZ4N8cQd9LXwB0t5G3sMaJ1rUyHozlSnWbEkxIp' && 1===2 ){
-        try{
+app.get('/liverates/:key', async (req, res) => {
+    if (req.params.key === 'f2PqY7RjVh6mKZ4N8cQd9LXwB0t5G3sMaJ1rUyHozlSnWbEkxIp' && 1 === 2) {
+        try {
             const response = await axios.get('https://api.eforexindia.com/Currency/json/Oh7JFPOzosq6VM8IYguWw1fCfVWVPuUCEtCC0mQfLXwf0GuhJ6/');
             res.json(response.data);
-        }catch(err){
-            console.error('Error fetching external api '+err.message);
-            res.status(500).json({Result:"failed",error:'Failed to provided rates'});
+        } catch (err) {
+            console.error('Error fetching external api ' + err.message);
+            res.status(500).json({ Result: "failed", error: 'Failed to provided rates' });
         }
-    }else{
-        res.status(404).json({error:'Not Found 404'});
+    } else {
+        res.status(404).json({ error: 'Not Found 404' });
     }
 });
 
-app.get('/liverates/:key/:isd', async(req,res)=>{
-    if(req.params.key==='f2PqY7RjVh6mKZ4N8cQd9LXwB0t5G3sMaJ1rUyHozlSnWbEkxIp'){
-        try{
-            const response = await axios.get('https://api.eforexindia.com/Currency/json/Oh7JFPOzosq6VM8IYguWw1fCfVWVPuUCEtCC0mQfLXwf0GuhJ6/'+req.params.isd.toUpperCase()+'/');
+app.get('/liverates/:key/:isd', async (req, res) => {
+    if (req.params.key === 'f2PqY7RjVh6mKZ4N8cQd9LXwB0t5G3sMaJ1rUyHozlSnWbEkxIp') {
+        try {
+            const response = await axios.get('https://api.eforexindia.com/Currency/json/Oh7JFPOzosq6VM8IYguWw1fCfVWVPuUCEtCC0mQfLXwf0GuhJ6/' + req.params.isd.toUpperCase() + '/');
             //console.log(response);
             //res.json(response.data);
             if (typeof response.data === "undefined") {
                 console.log("failed to provided rates 01");
-                res.status(500).json({Result:"failed",error:'failed to provided rates'});
-            }else{
+                res.status(500).json({ Result: "failed", error: 'failed to provided rates' });
+            } else {
                 let response1 = {};
-                if(response.data.message==='ok'){
+                if (response.data.message === 'ok') {
                     response1 = {
-                        "Result":response.data.Result,
-                        "message":response.data.message,
-                        "CurrencyCode":response.data.detail[0].CurrencyCode,
-                        "INRBuy":response.data.detail[0].INRBuy,
-                        "INRSell":response.data.detail[0].INRSell,
-                        "USDBuy":response.data.detail[0].USDBuy,
-                        "USDSell":response.data.detail[0].USDSell
+                        "Result": response.data.Result,
+                        "message": response.data.message,
+                        "CurrencyCode": response.data.detail[0].CurrencyCode,
+                        "INRBuy": response.data.detail[0].INRBuy,
+                        "INRSell": response.data.detail[0].INRSell,
+                        "USDBuy": response.data.detail[0].USDBuy,
+                        "USDSell": response.data.detail[0].USDSell
                     }
-                }else{
+                } else {
                     response1 = {
-                        "Result":response.data.Result,
-                        "message":response.data.message,
-                        "CurrencyCode":req.params.isd,
+                        "Result": response.data.Result,
+                        "message": response.data.message,
+                        "CurrencyCode": req.params.isd,
                     }
                 }
                 console.log(response1);
-                res.json(response1); 
+                res.json(response1);
             }
-            
-            
-        }catch(err){
-            console.log('Error while fetching live rates'+err.message);
-            res.status(500).json({Result:"failed",error:'failed to provided rates'});
+
+
+        } catch (err) {
+            console.log('Error while fetching live rates' + err.message);
+            res.status(500).json({ Result: "failed", error: 'failed to provided rates' });
         }
-    }else{
+    } else {
         console.log("404 - Not Found 02");
-        res.status(404).json({error:'Not Found 404'});
+        res.status(404).json({ error: 'Not Found 404' });
     }
 });
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ msg: 'Everything OK' });
+});
+
+app.get('/calculatecharges', (req, res) => {
+    res.status(200).json({ msg: 'Everything OK' });
+});
+
+app.get('/panvalidation', (req, res) => {
+    res.status(200).json({ msg: 'Everything OK' });
+});
+
 
 app.use((req, res) => {
     console.log("404 - Not Found 01");
     res.status(404).send("404 - Not Found");
-  });
+});
 
-app.listen(port,()=>{
-    console.log('rates app listing on '+port);
+app.listen(port, () => {
+    console.log('rates app listing on ' + port);
 })
